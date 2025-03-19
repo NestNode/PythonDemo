16096d67bc250126b7e7a60bce82a68efecbe74d
Linc <762699299@qq.com>
2025-03-19 20:32:55 +0800

    fix: pylint error

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+"""
+This is the main module for the Flask application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """
+    This function returns a simple HTTP response.
+    """
     return "Hello World! This is a simple HTTP response."
 
 if __name__ == '__main__':
